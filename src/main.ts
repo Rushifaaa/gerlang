7887a4e0c6f7be20c64c8993be010fe7871e80bb
Michael Federczuk <federczuk.michael@protonmail.com>
2022-11-15 18:53:30 +0100

    Add simple token lexing demo

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 // TODO
+
+import { Tokenizer } from "./Tokenizer";
+
+const tokenizer = new Tokenizer(process.stdin);
+
+process.stderr.write(
+	"enter expression: ",
+	(error) => {
+		if(error !== undefined) {
+			throw error;
+		}
+
+		tokenizer.getRemainingTokens()
+			.then(console.log.bind(console, "tokens:"));
+	}
+);
